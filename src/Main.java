9f287c4b74539d057fddef70e30ac5a54509f40c
zeynelabidintok <z.abidintok@gmail.com>
2022-08-29 00:24:48 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("selam dünya");
+        int result=(12<10)? 1:0;
+        System.out.println(result);
+    }
+}
