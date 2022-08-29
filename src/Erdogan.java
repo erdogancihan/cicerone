120cf1c66020289820b5a31e4cc01e02e9e814ac
zeynelabidintok <z.abidintok@gmail.com>
2022-08-29 11:12:26 +0200

    usage about some OOP things

--- src/Erdogan.java
+++ src/Erdogan.java
@@ -1,2 +1,20 @@
-public class Erdogan {
+private class Erdogan {
+
+
+    public Erdogan() {
+    }
+
+    public Erdogan(String name, String surname) {
+        System.out.println(name+surname);
+    }
+
+    public void newFunction(String name){
+        System.out.println(name);
+    }
+    public static void newFunction1(String name){
+        System.out.println(name);
+    }
+    public String newFunctionwithStringReturn(String name){
+        return name.concat(" ismi eklendi");
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
-
 public class Main {
     public static void main(String[] args) {
+        Erdogan er=new Erdogan();
         System.out.println("selam dünya");
         int result=(12<10)? 1:0;
         System.out.println(result);
         System.out.println("sonuc yazdirildi");
         System.out.println("github deneme");
         System.out.println("denememeeeee123");
+        er.newFunction("Ben erdogan");
+        Erdogan.newFunction1("erdogan cihan statik");
+        System.out.println(er.newFunctionwithStringReturn("ERDOGAN CIHAN"));
+        Erdogan er1=new Erdogan("Erdogan","Cihan");
     }
 }
