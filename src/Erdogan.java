c8f53efaa9aefb53e7df5f95a3f30cbc3a7b5500
erdogancihan <erdogancihan@icloud.com>
2022-08-30 12:58:29 +0200

    test

--- src/Erdogan.java
+++ src/Erdogan.java
@@ -1,20 +1,9 @@
 private class Erdogan {
-
-
-    public Erdogan() {
+  public Erdogan() {
+      helloWorld();
+    }
+    private void helloWorld(){
+        System.out.println("Hello World");
     }
 
-    public Erdogan(String name, String surname) {
-        System.out.println(name+surname);
-    }
-
-    public void newFunction(String name){
-        System.out.println(name);
-    }
-    public static void newFunction1(String name){
-        System.out.println(name);
-    }
-    public String newFunctionwithStringReturn(String name){
-        return name.concat(" ismi eklendi");
-    }
 }
