6f8c89945f41f7525f53425cd301449572df451a
erdogancihan <erdogancihan@icloud.com>
2022-09-04 17:36:19 +0200

    Primitive data Types.

--- src/Erdogan.java
+++ src/Erdogan.java
@@ -10,7 +10,8 @@ public class Erdogan {
         // learnIO();
         // formatOutput();
         // loopJob();
-        loopJob2();
+        // loopJob2();
+        dataTypes();
 
     }
 
@@ -125,6 +126,33 @@ public class Erdogan {
                 System.out.println("");
 
             }
+        }
+    }
+
+    private void dataTypes(){
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t>0) {
+            long x;
+
+            try {
+                x = sc.nextLong();
+                System.out.println(x +" can be fitted in:");
+                if(x>=-Math.pow(2,7)&& x<=Math.pow(2,7)-1){
+                    System.out.println("* byte");
+                }if(x>=-Math.pow(2,15)&& x<=Math.pow(2,15)-1){
+                    System.out.println("* short");
+                }
+                if(x>=-Math.pow(2,31)&& x<=Math.pow(2,31)-1){
+                    System.out.println("* int");
+                }
+                if(x>=-Math.pow(2,63)&& x<=Math.pow(2,63)-1){
+                    System.out.println("* long");
+                }
+            } catch (Exception e) {
+                System.out.println(sc.next() + " can't be fitted anywhere");
+            }
+        t--;
         }
     }
 
