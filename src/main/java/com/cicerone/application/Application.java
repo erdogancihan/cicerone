2af05e097f5f538825481af26ea1556892637c5e
erdogancihan <erdogancihan@icloud.com>
2022-11-09 20:11:27 +0100

    home test

--- src/main/java/com/cicerone/application/Application.java
+++ src/main/java/com/cicerone/application/Application.java
@@ -3,8 +3,11 @@ package com.cicerone.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
 
 	public static void main(String[] args) {
@@ -12,5 +15,10 @@ public class Application {
 		Translator translator= context.getBean(Translator.class);
 		translator.show();
 	}
-
+/*
+	@GetMapping
+	public String hello(){
+		return "hello World";
+	}
+*/
 }
